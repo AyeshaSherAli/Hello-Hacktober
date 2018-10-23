dae96aa9f895055b18082b47a91afd329ff3b417
AyeshaSherAli <43114900+AyeshaSherAli@users.noreply.github.com>
2018-10-23 03:42:59 -0700

    new way to show hello world in php

--- hello.php
+++ hello.php
@@ -1,3 +1,15 @@
 <?php 
 echo 'Hello World';
+
+// If you want to print in browser's console
+  print_r("Hello World");
+
+// or we can simply print it with print,the difference in print and echo is print has return value 1 and echo does not have return value  
+
+Print "Hello, World!";
+
+we can also print hello world with the help of veriables and var_demp function
+
+$stringVar = 'hello world';
+  var_dump($stringVar);
 ?>
